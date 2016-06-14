94ddc2832cf13d29ca67af53fee1eef2232ab77b
Carl Unsworth <carlunsworthuk@gmail.com>
2016-06-14 18:20:26 +0100

    * Initial commit of control software for LN2 Autofill system. * Details of system contained in README.md. * Completed initial evaluation version of microcontroller code. * Functionality includes: 	* Uses Arduino Bridge library, all external control through Http requests. 	* Configuration of pins, line numbers, thresholds, etc is done in global variables. 	* Functions to read/write to pins/valves, read LED voltage drops, produce a report on the status of all lines, and initiate an autofill. 	* Results of commands/autofills are returned to client as http response.
    * Python control software.
    	* Initiate autofills at scheduled times.
    	* Email results of autofill.
    	* Detect range of error conditions and send warning emails
    	* Record logs of all activity.
    	* Create plots of Volts vs time for each fill, and total fill duration vs time of fill for all lines.
    * Evaluation using real hardware components and adjustment of code if required.
    * Easier way to add/remove fill lines (rather than recomiling arduino code each time).

---
+++ LN2Fill.ino
@@ -0,0 +1,462 @@
+#include <Bridge.h>
+#include <BridgeServer.h>
+#include <BridgeClient.h>
+
+/* ----------------------------------------
+Liverpool Nucleasr Physics LN2 Fill System
+
+Arduino bridge code to recieve commands
+from python based server code running
+elsewhere
+
+List of commands to be implemented:
+  * Open/Close valve to tank
+  * Open/Close LN2 line
+  * Read LED potential (for given line number)
+  * Read valve state (for given line number)
+  * Initiate fill (line numer)
+  * Show staus of each line, including:
+    * LED V
+    * Valve Status
+    * Last fill success/fail and if success then duration
+---------------------------------------- */
+
+// Global variables
+
+BridgeServer Server;  // server for handling requests/commands
+
+const short SupplyTankPin = 2;
+const short NumFillLines = 4;
+
+const char * LineNames[] = {
+    "Test1",
+    "Test2",
+    "Test3",
+    "Test4"
+};
+
+const short LineValvePins[] = {
+  8,9,10,11
+};
+
+const short LineLedPins[] = {
+  0,1,2,3
+};
+
+const float LineLedThresh[] = {
+  3.0, 3.0, 3.0, 3.0
+};
+
+const int LineActive[] = {
+  1, 0, 0, 0
+};
+
+int LineStatus[] = {
+  0, 0, 0, 0
+};
+
+const short FillHoldTime = 20; // seconds, LED over threshold for this long for success
+const short FillTimeout = 300; // seconds
+
+// Setup and main loop
+// ----------------------------------
+void setup() {
+
+  // Initialise seriel comms
+  Serial.begin(9600);
+
+  // Initialise pins
+  int Pin, Line;
+  pinMode(SupplyTankPin,OUTPUT);   // Main supply valve control
+  for(Line = 0; Line < NumFillLines; Line++) {  // Individual line valves
+    Pin = LineValvePins[Line];
+    pinMode(Pin,OUTPUT);
+  }
+  pinMode(13,OUTPUT); // General status LED.
+
+  // Initialise bridge library and start a
+  // server to listen for commands
+  Bridge.begin();
+  Server.listenOnLocalhost();
+  Server.begin();
+
+}
+
+void loop() {
+
+  // Get clients coming from server
+  BridgeClient Client = Server.accept();
+
+  // Is there a new client?
+  if (Client) {
+    // Process request
+    process(Client);
+    // Close connection and free resources.
+    Client.stop();
+  }
+
+  // Delay until next loop
+  delay(50); // Poll every 50ms
+
+}
+
+// Process request
+// ----------------------------------
+
+void process(BridgeClient Client) {
+  // read the Command
+  String Command = Client.readStringUntil('/');
+
+  // Direct open/close valve commands
+  if (Command == "openline") {
+    openline(Client);
+  }
+  if (Command == "closeline") {
+    closeline(Client);
+  }
+  if (Command == "opentank") {
+    opentank(Client);
+  }
+  if (Command == "closetank") {
+    closetank(Client);
+  }
+
+  // Direct read of line status
+  if(Command == "readled") {
+    readled(Client);
+  }
+  if(Command == "readvalve") {
+    readvalve(Client);
+  }
+  
+  // Initiiate fill cycle commands
+  if(Command == "fillline") {
+    fillline(Client);
+  }
+
+  // Line status command
+  if (Command == "readstatus") {
+    readstatus(Client);
+  }
+
+  // Following commands are from the example code I used for building this - CU
+  // is "digital" Command?
+  if (Command == "digital") {
+    digitalCommand(Client);
+  }
+
+  // is "analog" Command?
+  if (Command == "analog") {
+    analogCommand(Client);
+  }
+
+}
+
+
+// Functions for directly opening/closing valves
+// -----------------------------------------------
+
+void openline(BridgeClient Client) {
+  int LineNumber, Pin;
+
+  LineNumber = Client.parseInt();
+
+  if (LineNumber > NumFillLines) {
+    Client.print(F("Unrecognised Line Number!"));
+    return;
+  }
+
+  Pin = LineValvePins[LineNumber - 1];
+
+  digitalWrite(Pin,1);
+
+  Client.print(F("Opening line "));
+  Client.print(LineNumber);
+  Client.print(F(" (Pin"));
+  Client.print(Pin);
+  Client.print(F(")..."));
+}
+
+void closeline(BridgeClient Client) {
+  int LineNumber, Pin;
+
+  LineNumber = Client.parseInt();
+
+  if (LineNumber > NumFillLines) {
+    Client.print(F("Unrecognised Line Number!"));
+    return;
+  }
+
+  Pin = LineValvePins[LineNumber - 1];
+
+  digitalWrite(Pin,0);
+
+  Client.print(F("Closing line "));
+  Client.print(LineNumber);
+  Client.print(F(" (Pin"));
+  Client.print(Pin);
+  Client.print(F(")..."));
+}
+
+void opentank(BridgeClient Client) {
+
+  digitalWrite(SupplyTankPin,1);
+
+  Client.print(F("Opening main supply tank (Pin "));
+  Client.print(SupplyTankPin);
+  Client.print(F(")..."));
+}
+
+void closetank(BridgeClient Client) {
+
+  digitalWrite(SupplyTankPin,0);
+
+  Client.print(F("Closing main supply tank (Pin "));
+  Client.print(SupplyTankPin);
+  Client.print(F(")..."));
+}
+
+
+// Functions for reading states
+// ----------------------------------
+
+void readled(BridgeClient Client) {
+  int LineNumber, Pin, Value ;
+
+  LineNumber = Client.parseInt();
+
+  if (LineNumber > NumFillLines) {
+    Client.print(F("Unrecognised Line Number!"));
+    return;
+  }
+
+  Pin = LineLedPins[LineNumber -1];
+  Value = analogRead(Pin);
+
+  Client.print(F("Line "));
+  Client.print(LineNumber);
+  Client.print(F(" (pin "));
+  Client.print(Pin);
+  Client.print(F(") reads "));
+  Client.print(Value);
+  Client.print(F(" ("));
+  Client.print(Adc2Volts(Value));
+  Client.print(F(" Volts)"));
+}
+
+void readvalve(BridgeClient Client) {
+  int LineNumber, Pin, Value ;
+
+  LineNumber = Client.parseInt();
+
+  if (LineNumber > NumFillLines) {
+    Client.print(F("Unrecognised Line Number!"));
+    return;
+  }
+
+  Pin = LineValvePins[LineNumber - 1];
+  Value = digitalRead(Pin);
+
+  Client.print(F("Valve for line "));
+  Client.print(LineNumber);
+  Client.print(F(" is "));
+  Client.print(Value==1 ? "Open." : "Closed.");
+}
+
+
+void readstatus(BridgeClient Client){
+  // This function should return a table showin g status of all lines
+  Client.print(F("This function should return status of all lines\n"));
+  Client.print(F("Not done yet...\n"));
+
+  // Write headings...
+  Client.print(F("# University of Liverpool - Nuclear Physics - LN2 Fill System\n\n"));
+  Client.print(F("# Full Fill-line Status Report:\n"));
+  Client.print(F("# LineNum\tActive?\tLED Pin\tLED Thresh\tADC val\tLED V\tValve Pin\tValve Status\tLast Fill Status\t\n\n"));
+
+  int i;
+  char StatusMessage[256];
+  int AdcVal;
+  bool ValveOpen;
+  int Status;
+  
+  for (i=0;i<NumFillLines;i++) {
+
+    AdcVal = analogRead(LineLedPins[i]);
+    ValveOpen = digitalRead(LineValvePins[i]);
+    Status = LineStatus[i];
+    
+    sprintf(StatusMessage,"%d\t\t%s\t%d\t%04f\t\t%d\t%f\t%d\t\t%s\t\t%s\n",i,LineActive[i]==1?"Yes":"No",LineLedPins[i],LineLedThresh[i],\
+        AdcVal,Adc2Volts(AdcVal),LineValvePins[i],ValveOpen?"Open":"Closed",LineStatus[i]?"Success!":"Fail!");
+
+    Client.print(StatusMessage);
+  }
+
+
+  
+}
+
+// Functions for initiating/managing fill cylce
+// -----------------------------------------------
+
+// Fill a single line, 
+int fillline(BridgeClient Client) {
+  int LineNumber;
+  int FillTime = 0;
+
+  float LedVolts = 0.0;
+
+  LineNumber = Client.parseInt();
+
+  if (LineNumber > NumFillLines) {
+    Client.print(F("Unrecognised Line Number!"));
+    return -1;
+  }
+
+  Client.print(F("Filling line "));
+  Client.print(LineNumber);
+  Client.print(F("Please wait...\n\n"));
+  Client.print(F("Time(s)\tLED Volts\tStatus\n"));
+
+  short ValvePin = LineValvePins[LineNumber - 1];
+  short LedPin = LineLedPins[LineNumber - 1];
+
+  // Open main valve to tank
+  digitalWrite(SupplyTankPin,1);
+  
+  // Open valve to line
+  digitalWrite(ValvePin,1);
+
+  int LedOverThreshTime = 0;
+  // Monitor LED and output value until success or timeout
+  while (FillTime < FillTimeout) {
+
+    LedVolts = Adc2Volts(analogRead(LedPin));
+
+    if (LedVolts > LineLedThresh[LineNumber - 1]) {
+      LedOverThreshTime += 1;
+    } else {
+      LedOverThreshTime = 0;
+    }
+
+    if (LedOverThreshTime >= FillHoldTime) {
+      Client.print(F("Fill complete, closing valves..."));
+      break;
+    }
+
+    Client.print(FillTime);
+    Client.print("\t");
+    Client.print(LedVolts);
+    Client.print("\t");
+    Client.print(LedVolts > LineLedThresh[LineNumber - 1] ? "Cold\n" : "Warm\n");
+
+    Client.flush();
+
+    FillTime += 1;
+    
+    delay(1000); // Wait 1s
+    
+  }
+
+  if (FillTime < FillTimeout) {
+    Client.print("\nSuccess!!!");
+    LineStatus[LineNumber - 1] = 1;
+  }
+  else {
+    Client.print("\nFail!!!");
+    LineStatus[LineNumber -1] = 0;
+  }
+  
+  // Close valve
+  digitalWrite(ValvePin,0);
+
+  // Close tank
+  digitalWrite(SupplyTankPin,0);
+
+  // return fill time
+  return FillTime;
+}
+
+// Fill all active lines
+int fillall(BridgeClient Client) {
+  
+}
+
+// Helper functions
+// ----------------------------------
+
+float Adc2Volts(int AdcVal) {
+  // This function should return ADV Volts for input of ADC value/bin.
+  // Roughly works for now, needs proper calibration
+  return AdcVal * 0.00495;
+}
+
+
+// Functions for commands from the bridge lib example are below:
+// --------------------------------------------------------------------
+void digitalCommand(BridgeClient Client) {
+  int pin, value;
+
+  // Read pin number
+  pin = Client.parseInt();
+
+  // If the next character is a '/' it means we have an URL
+  // with a value like: "/digital/13/1"
+  if (Client.read() == '/') {
+    value = Client.parseInt();
+    digitalWrite(pin, value);
+  } else {
+    value = digitalRead(pin);
+  }
+
+  // Send feedback to Client
+  Client.print(F("Pin D"));
+  Client.print(pin);
+  Client.print(F(" set to "));
+  Client.println(value);
+
+  // Update datastore key with the current pin value
+  String key = "D";
+  key += pin;
+  Bridge.put(key, String(value));
+}
+
+void analogCommand(BridgeClient Client) {
+  int pin, value;
+
+  // Read pin number
+  pin = Client.parseInt();
+
+  // If the next character is a '/' it means we have an URL
+  // with a value like: "/analog/5/120"
+  if (Client.read() == '/') {
+    // Read value and execute command
+    value = Client.parseInt();
+    analogWrite(pin, value);
+
+    // Send feedback to Client
+    Client.print(F("Pin D"));
+    Client.print(pin);
+    Client.print(F(" set to analog "));
+    Client.println(value);
+
+    // Update datastore key with the current pin value
+    String key = "D";
+    key += pin;
+    Bridge.put(key, String(value));
+  } else {
+    // Read analog pin
+    value = analogRead(pin);
+
+    // Send feedback to client
+    Client.print(F("Pin A"));
+    Client.print(pin);
+    Client.print(F(" reads analog "));
+    Client.println(value);
+
+    // Update datastore key with the current pin value
+    String key = "A";
+    key += pin;
+    Bridge.put(key, String(value));
+  }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# University of Liverpool - Nuclear Physics Laboratory - LN2 Autofill System
+
+This repository contains the code for the University of Liverpool Nuclear Physics LN2 autofill system.
+
+## Hardware Components
+
+* Analogue
+  * HLMP-1710 green LEDs - Mounted in LN2 exhaust valve, resistance increases when temperature drops so able to tell when Dewar is full.
+  * 6.8 kOhm resistors - In series with LEDs to control current.
+  * 24V power supply
+  * Voltage dividers - 500 Ohm and 4 kOhm to scale potential into 5V range of Arduino ADC.
+  * Relay board (Sainsmart 8ch Relay Board)
+* Digital
+  * Arduino Yun - Contains:
+    * Atmel ATmega32U4 microcontroller
+    * Atheros AR9331 running OpenWRT wireless stack
+    * 6 x 10bit 5V ADCs
+    
+## Software Components
+
+* System consists of two separate codes:
+  * C code to run on Arduino microcontroller.
+	* Uses Arduino Bridge Library to communicate via HTTP requests.
+	* Allows direct read/write of pins via webbrowser for debugging.
+	* "Filllines" defined based on relay pin number, LED aDC number, name, threshold for ADC/LED.
+	* Several built-in functions to read/write properties of each line or initiate a fill cycle and report the results.
+  * Python script to run on AR9331 or another computer and send control signals.
+	* Contains schedule for when lines should be filled, sends fill command to microcontroller when time comes.
+	* Automatically log all fills.
+	* Plot LED volts vs time for latest fill.
+	* Plot total fill time for all historical fills.
+	* Send email success/fail messages for all autofills.
+	* Detect other fail conditions such as no response from Arduino and email warnings.
+
+
+## Authors
+
+Carl Unsworth
+Dan Judson
+Design of LED based LN2 flow detection copied from TRIUMF GRSI group. 
